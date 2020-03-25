08101fb12c786cd32fea183485da6efe74c3b878
Nick Whitney <ubersentinel@gmail.com>
2020-03-25 13:33:28 -0400

    Update example to get a closer look at the output
    Need to get a closer look at the output to see the discontinuities in the noise before fixing opensimplex.

--- examples/open_simplex.rs
+++ examples/open_simplex.rs
@@ -2,7 +2,7 @@
 
 extern crate noise;
 
-use noise::{utils::*, OpenSimplex, Seedable};
+use noise::{utils::*, OpenSimplex, ScaleBias, Seedable};
 
 fn main() {
     let open_simplex = OpenSimplex::new();
@@ -13,7 +13,16 @@ fn main() {
 
     let open_simplex = open_simplex.set_seed(1);
 
-    PlaneMapBuilder::new(&open_simplex)
-        .build()
-        .write_to_file("open_simplex_seed=1.png");
+    let scaled = ScaleBias::new(&open_simplex).set_scale(2.0);
+
+    let noise_map = PlaneMapBuilder::new(&scaled)
+        .set_size(1024, 1024)
+        .set_x_bounds(-2.0, 2.0)
+        .set_y_bounds(-2.0, 2.0)
+        .build();
+
+    ImageRenderer::new()
+        .set_gradient(ColorGradient::new().build_rainbow_gradient())
+        .render(&noise_map)
+        .write_to_file("open_simplex_rainbow.png");
 }
